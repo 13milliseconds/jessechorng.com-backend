67b434afb307d4d2823e592ffaf6c31dfb94bb8d
Francois Huyghe <francois@13milliseconds.com>
2021-12-15 18:48:00 -0500

    debug

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,23 @@
 const { parse } = require("pg-connection-string");
-const { host, port, database, user, password } = parse(process.env("DATABASE_URL"));
 
-module.exports = ({ env }) => ({
-    connection: {
-      client: 'postgres',
-      connection: {
-        host,
-        port,
-        database,
-        user,
-        password,
-        ssl: { rejectUnauthorized: false },
-      },
-      debug: false,
-    },
-});
+module.exports = ({ env }) => {
+    const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+    
+    return {
+        connection: {
+            client: 'postgres',
+            connection: {
+                host,
+                port,
+                database,
+                user,
+                password,
+                ssl: { rejectUnauthorized: false },
+            },
+            debug: false,
+        },
+    }
+};
   
 
 // module.exports = ({ env }) => {
