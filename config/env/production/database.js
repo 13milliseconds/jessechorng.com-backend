781c6b8a5c240d6a75de8ea12d3659da8d6cfc5c
Francois Huyghe <francois@13milliseconds.com>
2021-12-15 18:45:51 -0500

    custom database setup based on new docs

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,41 @@
 const { parse } = require("pg-connection-string");
+const { host, port, database, user, password } = parse(process.env("DATABASE_URL"));
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false }
-        },
-        options: {
-          ssl: false
-        },
+module.exports = ({ env }) => ({
+    connection: {
+      client: 'postgres',
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
       },
+      debug: false,
     },
-  };
-};
+});
+  
+
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+
+//   return {
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "bookshelf",
+//         settings: {
+//           client: "postgres",
+//           host,
+//           port,
+//           database,
+//           username: user,
+//           password,
+//           ssl: { rejectUnauthorized: false },
+//         },
+//         options: {},
+//       },
+//     },
+//   };
+// };
